d234e2f5bc5e6201e412abf76d4c80e9d39feade
Yusuke Shinyama <yusuke@shinyama.jp>
2018-01-08 15:39:14 +0900

    Added: leafs.

--- tools/learn.py
+++ tools/learn.py
@@ -9,8 +9,7 @@ import sys
 from math import log2
 from comment import CommentEntry
 
-# getetp: calc the entropy.
-def getetp(keys):
+def calcetp(keys):
     n = sum(keys.values())
     etp = sum( v*log2(n/v) for v in keys.values() ) / n
     return etp
@@ -25,8 +24,18 @@ def countkeys(ents):
             d[k] = 1
     return d
 
+def bestkey(keys):
+    maxkey = None
+    maxv = 0
+    for (k,v) in keys.items():
+        if maxkey is None or maxv < v:
+            maxkey = k
+            maxv = v
+    assert maxkey is not None
+    return maxkey
+
 def entetp(ents):
-    return getetp(countkeys(ents))
+    return calcetp(countkeys(ents))
 
 
 ##  Feature
@@ -108,10 +117,26 @@ class TreeBranch:
                 (self.feature, self.arg))
 
     def dump(self, depth=0):
-        ind = ' '*depth
+        ind = '  '*depth
         print ('%sBranch %r: %r' % (ind, self.feature, self.arg))
         for branch in self.children:
             branch.dump(depth+1)
+        return
+
+##  TreeLeaf
+##
+class TreeLeaf:
+
+    def __init__(self, key):
+        self.key = key
+        return
+
+    def __repr__(self):
+        return ('<TreeLeaf(%r)>' % (self.key))
+
+    def dump(self, depth=0):
+        ind = '  '*depth
+        print ('%sLeaf %r' % (ind, self.key))
         return
 
 
@@ -119,18 +144,30 @@ class TreeBranch:
 ##
 class TreeBuilder:
 
-    def __init__(self, features, minkeys=2, minent=0.01, debug=1):
-        self.features = features
+    FEATURES = (
+        QF('deltaLine'),
+        QF('deltaCols'),
+    )
+
+    name2feat = { feat.name: feat for feat in FEATURES }
+
+    def __init__(self, names, minkeys=2, minent=0.01, debug=1):
+        self.features = [ self.getfeat(name) for name in names ]
         self.minkeys = minkeys
         self.minent = minent
         self.debug = debug
         return
 
+    @classmethod
+    def getfeat(klass, name):
+        return klass.name2feat[name]
+
     def build(self, ents, depth=0):
         keys = countkeys(ents)
-        etp = getetp(keys)
+        etp = calcetp(keys)
+        ind = '  '*depth
         if self.debug:
-            print ('Build:', keys, etp)
+            print ('%sBuild: %r, %.3f' % (ind, keys, etp))
         if len(ents) < 2: return None
         if len(keys) < self.minkeys: return None
         if etp < self.minent: return None
@@ -146,16 +183,39 @@ class TreeBuilder:
         if minbranch is None: return None
         (feat, arg, split) = minbranch
         if self.debug:
-            print ('Feature:', feat, arg, etp)
-            for (i,ents) in enumerate(split):
-                r = [ (e[feat.name], e.key) for e in ents ]
-                print (' Split%d (%d): %r' % (i, len(r), r))
+            print ('%sFeature: %r %r, %.3f' % (ind, feat, arg, etp))
         children = []
-        for es in split:
+        for (i,es) in enumerate(split):
+            if 2 <= self.debug:
+                r = [ (e[feat.name], e.key) for e in es ]
+                print ('%s Split%d (%d): %r' % (ind, i, len(r), r))
             branch = self.build(es, depth+1)
-            if branch is not None:
+            if branch is None:
+                keys = countkeys(es)
+                if self.debug:
+                    print ('%s Leaf: %r' % (ind, keys))
+                children.append(TreeLeaf(bestkey(keys)))
+            else:
                 children.append(branch)
         return TreeBranch(feat, arg, children)
+
+
+# export_tree
+def export_tree(tree):
+    if isinstance(tree, TreeBranch):
+        children = [ export_tree(branch) for branch in tree.children ]
+        return (tree.feature.name, tree.arg, children)
+    else:
+        return (tree.key)
+
+# import_tree
+def import_tree(builder, tree):
+    if isinstance(tree, tuple):
+        (name, arg, children) = tree
+        children = [ import_tree(builder, branch) for branch in children ]
+        return TreeBranch(builder.getfeat(name), arg, children)
+    else:
+        return TreeLeaf(tree)
 
 
 # main
@@ -169,9 +229,10 @@ def main(argv):
         e['deltaLine'] = int(e['line']) - int(e.get('prevLine',0))
         e['deltaCols'] = int(e['cols']) - int(e.get('prevCols',0))
         ents.append(e)
-    builder = TreeBuilder([QF('deltaLine'), QF('deltaCols')])
+    builder = TreeBuilder(['deltaLine', 'deltaCols'])
     root = builder.build(ents)
     root.dump()
+    print (export_tree(root))
     return 0
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
