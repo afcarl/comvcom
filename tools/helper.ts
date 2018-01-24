9ddae2b0e40dfd37ce584f29f68546926859cc80
Yusuke Shinyama <yusuke@shinyama.jp>
2018-01-25 00:09:52 +0900

    Changed: helper.ts

--- tools/helper.ts
+++ tools/helper.ts
@@ -1,7 +1,10 @@
 // helper.ts
 
 const FIELDNAME = 'CommentTaggerData';
-const CHOICES = [
+const TARGETS = [
+    'L:Left', 'R:Right', 'U:Up', 'X:In-place', 'Z:Other'
+];
+const CATEGORIES = [
     'm:Meta Info.', 'o:Value Desc.',
     'a:Precondition', 'p:Postcondition',
     't:Type/Enum/Iface', 'i:Instruction', 'g:Guide',
@@ -12,18 +15,25 @@ const CHOICES = [
 var textarea: HTMLTextAreaElement = null;
 
 class Item {
-    ctype: string = '';
-    ung: boolean = false;
+
+    beginning: boolean = false;
+    target: string = '';
+    category: string = '';
+    ungrammatical: boolean = false;
     updated: number = 0;
 
     load(cols: string[]) {
-        this.ctype = cols[0];
-        this.ung = (cols[1] == 'true');
-        this.updated = parseInt(cols[2]);
+        this.beginning = (cols[0] == 'true');
+        this.target = cols[1];
+        this.category = cols[2];
+        this.ungrammatical = (cols[3] == 'true');
+        this.updated = parseInt(cols[4]);
     }
 
     save() {
-        return this.ctype+','+this.ung+','+this.updated;
+        return (this.beginning+','+this.target+','+
+                this.category+','+this.ungrammatical+','+
+                this.updated);
     }
 }
 
@@ -70,13 +80,21 @@ function updateHTML(data: ItemList) {
     let cids = Object.getOwnPropertyNames(data);
     for (let cid of cids) {
         let item = data[cid];
-        let select = document.getElementById('S'+cid) as HTMLSelectElement;
-        if (select !== null) {
-            select.value = item.ctype;
+        let cb1 = document.getElementById('CB'+cid) as HTMLInputElement;
+        if (cb1 !== null) {
+            cb1.checked = item.beginning;
         }
-        let checkbox = document.getElementById('C'+cid) as HTMLInputElement;
-        if (checkbox !== null) {
-            checkbox.checked = item.ung;
+        let sel1 = document.getElementById('ST'+cid) as HTMLSelectElement;
+        if (sel1 !== null) {
+            sel1.value = item.target;
+        }
+        let sel2 = document.getElementById('SC'+cid) as HTMLSelectElement;
+        if (sel2 !== null) {
+            sel2.value = item.category;
+        }
+        let cb2 = document.getElementById('CG'+cid) as HTMLInputElement;
+        if (cb2 !== null) {
+            cb2.checked = item.ungrammatical;
         }
     }
 }
@@ -90,11 +108,31 @@ function saveText() {
 function initData(): ItemList {
     let data: ItemList = {};
 
-    function onCtypeChanged(ev: Event) {
-        let e = ev.target as HTMLSelectElement;
-        let cid = e.id.substr(1);
+    function onBChanged(ev: Event) {
+        let e = ev.target as HTMLInputElement;
+        let cid = e.id.substr(2);
         let item = data[cid];
-        item.ctype = e.value;
+        item.beginning = e.checked;
+        item.updated = Date.now();
+        textarea.value = exportText(data);
+	saveText();
+    }
+
+    function onTargetChanged(ev: Event) {
+        let e = ev.target as HTMLSelectElement;
+        let cid = e.id.substr(2);
+        let item = data[cid];
+        item.target = e.value;
+        item.updated = Date.now();
+        textarea.value = exportText(data);
+	saveText();
+    }
+
+    function onCategoryChanged(ev: Event) {
+        let e = ev.target as HTMLSelectElement;
+        let cid = e.id.substr(2);
+        let item = data[cid];
+        item.category = e.value;
         item.updated = Date.now();
         textarea.value = exportText(data);
 	saveText();
@@ -102,19 +140,18 @@ function initData(): ItemList {
 
     function onUngChanged(ev: Event) {
         let e = ev.target as HTMLInputElement;
-        let cid = e.id.substr(1);
+        let cid = e.id.substr(2);
         let item = data[cid];
-        item.ung = e.checked;
+        item.ungrammatical = e.checked;
         item.updated = Date.now();
         textarea.value = exportText(data);
 	saveText();
     }
 
-    for (let e of toArray(document.getElementsByClassName('ui'))) {
-        let cid = e.id;
+    function makeSelect(e: Element, id: string, choices: string[]) {
         let select = document.createElement('select');
-        select.id = 'S'+cid;
-        for (let k of CHOICES) {
+        select.id = id;
+        for (let k of choices) {
             let i = k.indexOf(':');
             let option = document.createElement('option');
             let v = k.substr(0, i);
@@ -122,17 +159,35 @@ function initData(): ItemList {
             option.innerText = v+')'+k.substr(i+1);
             select.appendChild(option);
         }
+        e.appendChild(select);
+        return select;
+    }
+
+    function makeCheckbox(e: Element, id: string, caption: string) {
         let label = document.createElement('label');
         let checkbox = document.createElement('input');
-        checkbox.id = 'C'+cid;
+        checkbox.id = id;
         checkbox.setAttribute('type', 'checkbox');
         label.appendChild(checkbox);
-        label.appendChild(document.createTextNode('Ung?'));
-        e.appendChild(select);
-        e.appendChild(document.createTextNode(' '));
+        label.appendChild(document.createTextNode(' '+caption));
         e.appendChild(label);
-        select.addEventListener('change', onCtypeChanged);
-        checkbox.addEventListener('change', onUngChanged);
+        return checkbox;
+    }
+
+    for (let e of toArray(document.getElementsByClassName('ui'))) {
+        let cid = e.id;
+        let cb1 = makeCheckbox(e, 'CB'+cid, 'B?');
+        cb1.addEventListener('change', onBChanged);
+        e.appendChild(document.createTextNode(' '));
+        let sel1 = makeSelect(e, 'ST'+cid, TARGETS);
+        sel1.addEventListener('change', onTargetChanged);
+        e.appendChild(document.createTextNode(' '));
+        let sel2 = makeSelect(e, 'SC'+cid, CATEGORIES);
+        sel2.addEventListener('change', onCategoryChanged);
+        e.appendChild(document.createTextNode(' '));
+        let cb2 = makeCheckbox(e, 'CG'+cid, 'Ung?');
+        cb2.addEventListener('change', onUngChanged);
+        e.appendChild(document.createTextNode(' '));
         data[cid] = new Item();
     }
     return data;
